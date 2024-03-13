1e727cab29e866efa3f002bf822fc01bf1b18000
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-03-13 17:59:19 -0300

    add a home

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [
+    BtnPrimaryComponent
+  ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent],
+  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent, FormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
